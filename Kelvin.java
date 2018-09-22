75c64a58329ede43ec23b2778a522a556fff3f16
Nandha kumar S <mailstonandhakumar@gmail.com>
2018-09-22 23:44:22 +0530

    Update and rename README.md to Kelvin.java

---
+++ Kelvin.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+  public class Kelvin{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter Celcius value:");
+      int c=sc.nextInt();
+      int k=c+273;
+      System.out.println("Kelvin:"+k);
+      
+    }
+  }   
